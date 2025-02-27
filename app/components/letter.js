9fd0cf8c62484f8f972b1b6c297f6e011f8eaf26
Sithu Htin <sithuhtin2022@gmail.com>
2025-02-27 19:28:14 +0700

    before form changed

--- app/components/letter.js
+++ app/components/letter.js
@@ -18,6 +18,7 @@ const WeddingSection = () => {
 
     return (
         <div className="flex flex-col md:flex-row h-screen bg-[#F5F5DC]">
+
             {/* Upper photo section (50% on mobile, 50% width on desktop/tablet) */}
             <div className="h-1/2 md:h-full md:w-1/2 w-full overflow-hidden md:pl-9">
                 <Image
@@ -28,8 +29,8 @@ const WeddingSection = () => {
                 />
             </div>
 
-            {/* Lower form section (50% on mobile, 50% width on desktop/tablet) */}
-            <div className="h-1/2 md:h-full md:w-1/2 w-full flex justify-center items-center bg-[#F5F5DC] p-4 md:p-6 font-satisfy">
+                        {/* Lower form section (50% on mobile, 50% width on desktop/tablet) */}
+                        <div className="h-1/2 md:h-full md:w-1/2 w-full flex justify-center items-center bg-[#F5F5DC] p-4 md:p-6 font-satisfy">
                 <form onSubmit={handleSubmit} className="w-full max-w-md space-y-4">
                     <textarea
                         value={wish}
@@ -54,6 +55,8 @@ const WeddingSection = () => {
                     </div>
                 </form>
             </div>
+
+
         </div>
     );
 };
