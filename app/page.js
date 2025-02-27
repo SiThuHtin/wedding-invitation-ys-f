aafbbb029605f0958bc7163dced3f5ac26d44e68
Sithu Htin <sithuhtin2022@gmail.com>
2025-02-27 18:43:33 +0700

    text-sm

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <br />
       It is when an imperfect couple learns to enjoy their differences"
     </p>
-    <div className="py-3 font-normal italic text-center">-Dave Meurer</div>
+    <div className="py-3 font-normal text-yellow-700 italic text-center">-Dave Meurer</div>
   </div>
 
   {/* Right Section - Image */}
@@ -54,25 +54,25 @@ export default function Home() {
           </div>
 
           <div className="md:w-1/2 space-y-4 mt-6 md:mt-0">
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>5:00 PM</span> <span>INVITE START TIME</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>5:30 PM</span> <span>SIGNING CEREMONY BEGINS</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>6:00 PM</span> <span>PINT O'CLOCK</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>6:45 PM</span> <span>WELCOME TO THE NEWLYWED</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>7:30 PM</span> <span>BUFFET OPENS</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>8:00 PM</span> <span>PARTY TIME</span>
             </div>
-            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-md md:text-lg flex justify-between">
+            <div className="border-t border-[#3F4A37] pt-3 text-[#3F4A37] text-sm md:text-lg flex justify-between">
               <span>9:00 PM</span> <span>RECEPTION ENDS</span>
             </div>
           </div>
